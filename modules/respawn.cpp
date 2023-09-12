4bae21c55f7381ec53d1cdd7869fffe6666f6105
Simon Boerwinkle <sboerwinkle@gmail.com>
2023-09-12 08:54:07 -0500

    Some polish on the respawner

--- modules/respawn.cpp
+++ modules/respawn.cpp
@@ -18,16 +18,26 @@ static void respawn_tick(gamestate *gs, ent *me) {
 	int32_t check = getSlider(me, 1);
 	int32_t delay = getSlider(me, 2);
 	list<player> *ps = gs->players;
+	player *toRevive = NULL;
 	range(i, ps->num) {
 		player *p = &(*ps)[i];
 		if (p->reviveCounter > delay && (p->data & mask) == check) {
-			p->entity = mkPlayer(gs, me->center, (p->data % 2) + 1);
-			mkEye(gs, p->entity);
-			p->reviveCounter = 0;
-			p->entity->color = p->color;
-			uSlider(me, 4, getSlider(me, 3));
-			return;
+			toRevive = p;
+			delay = p->reviveCounter;
 		}
+	}
+	if (!toRevive) return;
+
+	toRevive->entity = mkPlayer(gs, me->center, (toRevive->data % 2) + 1);
+	mkEye(gs, toRevive->entity);
+	toRevive->reviveCounter = 0;
+	toRevive->entity->color = toRevive->color;
+	uSlider(me, 4, getSlider(me, 3));
+
+	// Lets spawning tie into the logic wiring system,
+	// though for simplicity it's always just signal 0
+	wiresAnyOrder(w, me) {
+		pushBtn(w, 0);
 	}
 }
 
